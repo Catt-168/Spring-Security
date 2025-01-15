087cf9174727651d2f3b85fe2bb29ed6eb1efb2c
Catt-168 <satpaingthu2000@gmail.com>
2025-01-15 23:26:47 +0630

    feat: first commit

---
+++ src/main/java/com/kindergarten/SpringSecurityExercise/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.kindergarten.SpringSecurityExercise.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(customizer -> customizer.disable());
+        http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+//        http.formLogin(Customizer.withDefaults()); // Default login ui form
+        http.httpBasic(Customizer.withDefaults());
+//      generate new session id everytime, works with postman
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
+
+}
